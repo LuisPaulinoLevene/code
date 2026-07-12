757b1d0d648c8b709d7e35bf3825784fb0d5be08
LuisPaulinoLevene <136761438+LuisPaulinoLevene@users.noreply.github.com>
2026-07-12 05:02:40 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from decouple import config
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
 
 
 DATABASE_URL = config("DATABASE_URL")
@@ -30,4 +31,23 @@ engine = create_engine(
 )
 
 
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+
 Base = declarative_base()
+
+
+
+def get_db():
+
+    db = SessionLocal()
+
+    try:
+        yield db
+
+    finally:
+        db.close()
