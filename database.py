0f1c641584cbb25b0ab078fb1ea3bde49e3b3e62
LuisPaulinoLevene <136761438+LuisPaulinoLevene@users.noreply.github.com>
2026-07-12 04:56:22 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,34 +1,33 @@
 from decouple import config
-
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import declarative_base
 
 
 DATABASE_URL = config("DATABASE_URL")
 
 
+if DATABASE_URL.startswith("postgres://"):
+
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql+psycopg2://",
+        1
+    )
+
+
+elif DATABASE_URL.startswith("postgresql://"):
+
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgresql://",
+        "postgresql+psycopg2://",
+        1
+    )
+
+
 engine = create_engine(
     DATABASE_URL,
-    echo=True,
-)
-
-
-SessionLocal = sessionmaker(
-    bind=engine,
-    autocommit=False,
-    autoflush=False,
+    echo=True
 )
 
 
 Base = declarative_base()
-
-
-def get_db():
-
-    db = SessionLocal()
-
-    try:
-        yield db
-
-    finally:
-        db.close()
