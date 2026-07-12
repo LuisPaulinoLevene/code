88cabe7e497307c22d35e5ed18fa2c2521f3a662
LuisPaulinoLevene <136761438+LuisPaulinoLevene@users.noreply.github.com>
2026-07-12 05:15:58 +0200

    Update unlock.py

--- routers/pages/unlock.py
+++ routers/pages/unlock.py
@@ -1,20 +1,12 @@
-from fastapi import APIRouter, Request
+from fastapi import Request
 from fastapi.templating import Jinja2Templates
-
-
-router = APIRouter()
-
 
 templates = Jinja2Templates(directory="templates")
 
-
-
 @router.get("/")
 async def pagina_inicial(request: Request):
-
     return templates.TemplateResponse(
-        "dashboard.html",
-        {
-            "request": request
-        }
+        request=request,
+        name="dashboard.html",
+        context={}
     )
